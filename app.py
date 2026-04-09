d865c0c0d14427e7241bbb3dd3c1aad251279a0f
sejal <sejal@example.com>
2026-04-09 11:09:20 +0530

    Initial commit: add Streamlit student marks analyzer and requirements

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+# Title
+st.title("Student Marks Analyzer")
+
+# Input fields
+name = st.text_input("Enter Student Name")
+
+maths = st.number_input("Maths Marks", 0, 100)
+science = st.number_input("Science Marks", 0, 100)
+english = st.number_input("English Marks", 0, 100)
+
+# Button
+if st.button("Calculate Result"):
+
+    total = maths + science + english
+    percentage = total / 3
+
+    # Grade logic
+    if percentage >= 90:
+        grade = "A+"
+    elif percentage >= 75:
+        grade = "A"
+    elif percentage >= 60:
+        grade = "B"
+    elif percentage >= 50:
+        grade = "C"
+    else:
+        grade = "Fail"
+
+    # Output
+    st.subheader("Result")
+    st.write(f"Name: {name}")
+    st.write(f"Total Marks: {total}")
+    st.write(f"Percentage: {percentage:.2f}%")
+    st.write(f"Grade: {grade}")
